c1d4934e0b3549173549d3f3eedef51535b815f6
Jake Soenneker <jake@soeneker.com>
2024-12-22 13:58:56 +0000

    Tests

--- test/Soenneker.Dtos.StreetAddress.Tests/StreetAddressTests.cs
+++ test/Soenneker.Dtos.StreetAddress.Tests/StreetAddressTests.cs
@@ -1,5 +1,18 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
 namespace Soenneker.Dtos.StreetAddress.Tests;
 
-public class StreetAddressTests
+[Collection("Collection")]
+public class StreetAddressTests : FixturedUnitTest
 {
+    public StreetAddressTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
 }
